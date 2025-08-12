9f283593883bf8b11e77c964b78605d7cdcbe8a2
TanyaKovalenko <tanyakovalenko1994@gmail.com>
2025-08-12 10:39:32 +0300

    add new tool

--- src/main.py
+++ src/main.py
@@ -10,6 +10,16 @@ def add(a: int, b: int) -> int:
     """Adds two numbers together."""
     return a + b
 
+@mcp_server.tool
+def author_favorite_color(a: int) -> str:
+    """Says the author's favorite color."""
+    if a == 1:
+        return "Blue"
+    elif a == 2:
+        return "Red"
+    else:
+        return "Unknown"
+
 
 def main():
     mcp_server.run(transport="http", host=envs.MCP_HOST, port=envs.MCP_PORT)
